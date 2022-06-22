4015fa496d8bd375af34206d0e7d361f6f41acb0
RobinGJonsson <robin__94@hotmail.se>
2022-06-22 11:01:32 +0000

    declare functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function rungame() {
+
+}
+
+function checkAnswer() {
+    
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWorngAnswer() {
+    
+}
+
+function displayAdditionQuestion() {
+    
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
